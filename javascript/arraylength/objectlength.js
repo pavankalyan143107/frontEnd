955e9d15bdb6bf0162b0c97763b3f64f8eab8e85
pavankalyan143107 <suripakapavankalyan7@gmail.com>
2022-08-19 15:49:48 +0530

    okk

--- javascript/arraylength/objectlength.js
+++ javascript/arraylength/objectlength.js
@@ -1,20 +1,31 @@
 
 // To find object length, first we need to change object into array and then apply rule of array.
 
+let arr={
+    id:101,
+    name:"sai",
+    sal:68000
+}
 
-var arr={
+ console.log( Object.keys(arr).length);
+
+
+
+
+
+var arr1={
     id:102,
     name:"pavan",
     sal:45000
 
 }
 
-if(Object.keys.length){
+if(Object.keys.length>0){
 
     console.log("not a empty object");
 
 }
 else{
     console.log("empty object");
-}
+}  
 
