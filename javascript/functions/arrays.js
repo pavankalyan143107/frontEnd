4866a0a1c4784e3fd1c31c64ffa4ebbb7b12c2ef
pavankalyan143107 <suripakapavankalyan7@gmail.com>
2022-08-19 23:42:49 +0530

    events

--- javascript/functions/arrays.js
+++ javascript/functions/arrays.js
@@ -12,10 +12,10 @@ console.log(arr);
 
 //indexing  0  1  2  3            Note : index value should be starts with 0
 
-console.log(arr[0]);
-console.log(arr[1]);
-console.log(arr[2]);
-console.log(arr[3]);
+console.log(arr1[0]);
+console.log(arr1[1]);
+console.log(arr1[2]);
+console.log(arr1[3]);
 
 console.log(arr[10]);     // it is undefined.
 
@@ -25,25 +25,25 @@ console.log(arr[10]);     // it is undefined.
 // How to update.
 
 let arr2=["pavan","vamsi","sai","anand","subbu","seenu"]
-console.log(arr);
+console.log(arr2);
 
-console.log(arr[0]);
-console.log(arr[1]);
-console.log(arr[2]);
-console.log(arr[3]);
-console.log(arr[4]);
+console.log(arr2[0]);
+console.log(arr2[1]);
+console.log(arr2[2]);
+console.log(arr2[3]);
+console.log(arr2[4]);
 
-arr[1]="vamsi krishna"    // here updated.
+arr2[1]="vamsi krishna"    // here updated.
 
-console.log(arr);    
-
-
+console.log(arr2);    
 
 
-var arr=[30,40,50,60]
+
+
+var arr3=[30,40,50,60]
 
 for(i=0; i<=arr.length-1; i++){
-    console.log(arr[i]);
+    console.log(arr3[i]);
     
 
 }     
@@ -51,11 +51,11 @@ for(i=0; i<=arr.length-1; i++){
 
 
 
-var arr=[10,20,30,40,50,]
+var arr4=[10,20,30,40,50,]
 var i=0;
 
-while(i<=arr.length-1){
-    console.log(arr[i]);
+while(i<=arr4.length-1){
+    console.log(arr4[i]);
     i++;
 
 }      
@@ -63,9 +63,9 @@ while(i<=arr.length-1){
 
 
 
-let arr=[10,true,undefined,"pavan",[],{},10,20]
-console.log(arr);
-console.log(arr.length);          //here length is a property.          
+let arr5=[10,true,undefined,"pavan",[],{},10,20]
+console.log(arr5);
+console.log(arr5.length);          //here length is a property.          
 
            
 
